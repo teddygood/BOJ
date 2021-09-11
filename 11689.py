d6c165d01bb13f09d9158d70755b1f72b96c72d4
teddygood <ibear6954@gmail.com>
2021-09-11 20:58:10 +0900

    update 11689

---
+++ 11689.py
@@ -0,0 +1,11 @@
+n = int(input())
+result = n
+
+for i in range(2, round(n ** 0.5) + 1):
+    if n % i == 0:
+        while n % i == 0:
+            n //= i
+        result *= 1 - (1 / i) # Euler phi function
+if n > 1:
+    result *= 1 - (1 / n)
+print(round(result))
