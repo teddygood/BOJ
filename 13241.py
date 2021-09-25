1dc6a61e0937a6de8a473ce5b21a18272365f949
teddygood <ibear6954@gmail.com>
2021-09-25 15:11:28 +0900

    update 13241

---
+++ 13241.py
@@ -0,0 +1,12 @@
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a%b)
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+a, b = map(int, input().split())
+
+print(lcm(a, b))
