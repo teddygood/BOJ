a9c0e8d00069bb154c811feafc8c44503eff0c4c
teddygood <ibear6954@gmail.com>
2021-11-12 11:19:58 +0900

    update 1008

---
+++ 1008.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a = 0, b = 0;
+    cin >> a >> b;
+
+    cout.precision(10);
+    cout << static_cast<double>(a) / b << '\n';
+
+    return 0;
+}
