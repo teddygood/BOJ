51aa5e16a7b722f166c009624448122124780074
teddygood <ibear6954@gmail.com>
2022-07-07 06:13:39 +0000

    modify

--- 1181.py
+++ 1181.py
@@ -10,7 +10,7 @@ set_str_list = set(str_list)	# 중복값 제거
 str_list = list(set_str_list)	
 
 str_list.sort()
-str_list.sort(key = len)	# 문자열 길이 순으로 정렬
+str_list.sort(key=len)	# 문자열 길이 순으로 정렬
 
 for word in str_list:
     print(word)
