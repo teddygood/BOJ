f3c81c624c0a932c2f2d33e46344a33bc3b0d91a
teddygood <ibear6954@gmail.com>
2022-07-07 05:33:01 +0000

    update 1181

---
+++ 1181.py
@@ -0,0 +1,16 @@
+import sys
+
+n = int(sys.stdin.readline())
+str_list = []
+
+for i in range(n):
+    str_list.append(sys.stdin.readline().strip())
+
+set_str_list = set(str_list)	# 중복값 제거
+str_list = list(set_str_list)	
+
+str_list.sort()
+str_list.sort(key = len)	# 문자열 길이 순으로 정렬
+
+for word in str_list:
+    print(word)
