40c18a8a9f013150f1fb234351b9bc3c953d7e1e
teddygood <ibear6954@gmail.com>
2022-07-15 22:45:33 +0900

    update 11650

--- 11650.py
+++ 11650.py
@@ -1,17 +1,8 @@
-n = int(input())
-s = []
-
+import sys
+n = int(sys.stdin.readline())
+so = []
 for i in range(n):
-    start, end = map(int, input().split())
-    s.append([start, end])
-
-s = sorted(s, key=lambda a: a[0])
-s = sorted(s, key=lambda a: a[1])
-
-last = 0
-count = 0
-for i, j in s:
-    if i >= last:
-        count += 1
-        last = j
-print(count)
+    so.append(list(map(int, sys.stdin.readline().split())))
+so.sort(key=lambda x: (x[0], x[1]))
+for i in so:
+    print(i[0], i[1])
