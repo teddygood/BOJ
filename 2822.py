11be45d31ac158697bd726a20181d482aa77bf68
teddygood <ibear6954@gmail.com>
2022-08-02 20:12:07 +0900

    modify 2822

--- 2822.py
+++ 2822.py
@@ -1,16 +1,17 @@
 score = []
-for i in range(8):
+result = []
+score_sum = 0
+
+# score = [int(input()) for i in range(8)]
+
+for _ in range(8):
     score.append(int(input()))
-score_sort = sorted(score, reverse=True)
-big = []
-for i in range(5):
-    big.append(score_sort[i])
-sum = 0
-tmp = []
-for i in big:
-    sum += i
-    tmp.append(score.index(i))
-print(sum)
-tmp_s = sorted(tmp)
-for i in tmp_s:
-    print(i + 1, end=' ')
+
+for i in sorted(score, reverse=True)[:5]:
+    result.append(score.index(i))
+    score_sum += i
+
+print(score_sum)
+
+for i in sorted(result):
+    print(i+1, end=' ')
