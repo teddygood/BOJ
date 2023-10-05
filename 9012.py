8532ef76388455a404e4dbe91dced091a25e3d99
teddygood <ibear6954@gmail.com>
2023-10-05 17:17:08 +0900

    modify 9012

--- 9012.py
+++ 9012.py
@@ -1,19 +1,40 @@
+# t = int(input())
+#
+# for _ in range(t):
+#     parenthesis = list(input())
+#     sum = 0
+#
+#     for i in parenthesis:
+#         if i == "(":
+#             sum += 1
+#         elif i == ")":
+#             sum -= 1
+#         if sum < 0:
+#             print("NO")
+#             break
+#
+#     if sum > 0:
+#         print("NO")
+#     elif sum == 0:
+#         print("YES")
+
+
 t = int(input())
 
 for _ in range(t):
-    parenthesis = list(input())
-    sum = 0
+    stack = []
+    isVPS = True
+    for ch in input():
+        if ch == "(":
+            stack.append(ch)
+        else:
+            if stack:
+                stack.pop()
+            else:
+                isVPS = False
+                break
 
-    for i in parenthesis:
-        if i == "(":
-            sum += 1
-        elif i == ")":
-            sum -= 1
-        if sum < 0:
-            print("NO")
-            break
-    
-    if sum > 0:
-        print("NO")
-    elif sum == 0:
-        print("YES")
+    if stack:
+        isVPS = False
+
+    print("YES" if isVPS else "NO")
